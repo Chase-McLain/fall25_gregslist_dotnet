d25a13804e8ab78e5e186c3f1c014674a670f65a
Chase McLain <chasermclain@gmail.com>
2025-10-15 16:09:13 -0600

    car dates

--- Models/Car.cs
+++ Models/Car.cs
@@ -9,8 +9,32 @@ public class Car
   public DateTime UpdatedAt { get; set; }
   [MinLength(3), MaxLength(50)] public string Make { get; set; }
   [MinLength(1), MaxLength(100)] public string Model { get; set; }
+
+  private int? _year;
   // NOTE ? denotes that year will default to null instead of 0 now
-  [Range(1886, 2026)] public int? Year { get; set; }
+  [Range(1886, 9999)]
+  public int? Year
+  {
+
+    get
+    {
+      return _year;
+    }
+
+    set
+    {
+      int nextYear = new DateTime().Year + 1;
+      if (value > nextYear)
+      {
+        throw new ArgumentOutOfRangeException(value + $" is greater than the maximum year ({nextYear})");
+      }
+      if (value < 1886)
+      {
+        throw new ArgumentOutOfRangeException(value + $" is lower than the minimum year (1886)");
+      }
+      _year = value;
+    }
+  }
   [Range(0, 1000000)] public int? Price { get; set; }
   [Url, MaxLength(500)] public string ImgUrl { get; set; }
   [MaxLength(500)] public string Description { get; set; }
